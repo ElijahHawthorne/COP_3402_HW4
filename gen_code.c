211faf7d66b4bec80e4304374a31c5a3dc4b8d3e
Darel Gomez <darelg2212.sc@gmail.com>
2024-11-25 19:08:26 -0500

    Update gen_code.c

--- gen_code.c
+++ gen_code.c
@@ -54,6 +54,7 @@ static void gen_code_output_literals(BOFFILE bf)
 static void gen_code_output_program(BOFFILE bf, code_seq main_cs)
 {
     BOFHeader bfh = gen_code_program_header(main_cs);
+    
     bof_write_header(bf, bfh);
     gen_code_output_seq(bf, main_cs);
     gen_code_output_literals(bf);
@@ -67,23 +68,50 @@ void gen_code_program(BOFFILE bf, block_t prog)
 {
     code_seq main_cs = code_seq_empty();
     
-    // Write here
 
     // Start with variable declarations
+    code_seq_concat(&main_cs, gen_code_var_decls(prog.var_decls));
 
     // Then do statements (assign, call, if, while, read, print, block)
     code_seq_concat(&main_cs, gen_code_stmts(prog.stmts));
-
+    
     gen_code_output_program(bf, main_cs);
+}
+
+code_seq gen_code_var_decls(var_decls_t vds) {
+    code_seq ret = code_seq_empty();
+
+    var_decl_t* var_decl = vds.var_decls;
+    while(var_decl != NULL) {
+        code_seq_concat(&ret, gen_code_var_decl(*var_decl));
+    }
+
+    return ret;
+}
+
+code_seq gen_code_var_decl(var_decl_t vd) {
+    return gen_code_idents(vd.ident_list, vd.type_tag);
+}
+
+code_seq gen_code_idents(ident_list_t idents, AST_type t) {
+    code_seq ret = code_seq_empty();
+
+    ident_t* ident = idents.start;
+    printf("\nAST_type = %d", t);
+    while(ident != NULL) {
+        ident = ident->next;
+    }
+
+    return ret;
 }
 
 // Generate code for the list of statments given by stmts to out
 code_seq gen_code_stmts(stmts_t stmts) {
-    stmt_t* stmt = stmts.stmt_list.start;
-
     code_seq ret = code_seq_empty();
-
+    
+    stmt_t* stmt = stmts.stmt_list.start;
     while(stmt != NULL) {
+        // For some reason, *stmt causes a segmentation fault in hw4-gtest0.spl. This means stmt is not null but points to an invalid address. WHY
         code_seq_concat(&ret, gen_code_stmt(*stmt));
         stmt = stmt->next;
     }
@@ -92,7 +120,7 @@ code_seq gen_code_stmts(stmts_t stmts) {
 }
 
 code_seq gen_code_stmt(stmt_t stmt) {
-
+    
     switch(stmt.stmt_kind) {
         case assign_stmt: {
             return gen_code_assign_stmt(stmt.data.assign_stmt);
@@ -148,9 +176,7 @@ code_seq gen_code_read_stmt(read_stmt_t stmt) {
 }
 
 code_seq gen_code_print_stmt(print_stmt_t stmt) {
-    code_seq ret = gen_code_expr(stmt.expr);
-
-    return ret;
+    return gen_code_expr(stmt.expr);
 }
 
 code_seq gen_code_block_stmt(block_stmt_t stmt) {
@@ -195,6 +221,7 @@ code_seq gen_code_ident(ident_t id) {
 }
 
 code_seq gen_code_number(number_t num) {
+    
     unsigned int offset = literal_table_lookup(num.text, num.value);
     return code_seq_singleton(code_pint(GP, offset));
 }
