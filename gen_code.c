63d4de75865a4f88e8836485f22f28a93058179c
Darel Gomez <darelg2212.sc@gmail.com>
2024-11-27 01:39:49 -0500

    id_use

--- gen_code.c
+++ gen_code.c
@@ -135,7 +135,7 @@ code_seq gen_code_const_def(const_def_t cd) {
 
     code_seq_concat(&ret, gen_code_number(cd.number));
 
-    cd.ident.idu = id_use_create(cd.ident.idu->attrs, cd.ident.idu->levelsOutward);
+    cd.ident.idu = id_use_create(id_use_get_attrs(cd.ident.idu), cd.ident.idu->levelsOutward);
     int offset = id_use_get_attrs(cd.ident.idu)->offset_count;
     code_seq_add_to_end(&ret, code_swr(GP, offset, SP));
 
@@ -152,7 +152,7 @@ code_seq gen_code_idents(ident_list_t idents) {
 
     ident_t *ident = idents.start;
     while(ident != NULL){
-        ident->idu = id_use_create(ident->idu->attrs, ident->idu->levelsOutward);
+        ident->idu = id_use_create(id_use_get_attrs(ident->idu), ident->idu->levelsOutward);
         int offset = id_use_get_attrs(ident->idu)->offset_count;
         code_seq_add_to_end(&ret, code_swr(GP, offset, SP));
         ident = ident->next;
